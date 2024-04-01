b88da3c6ed3af4526587ff824033b492ced58e17
Carlos Aguilera <ckarlos_95@hotmail.es>
2024-04-01 11:40:10 -0600

    Change default message again

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello from Cloud Run").listen(process.env.PORT ?? 8080);
+const app = new Elysia().get("/", () => "Hello again from Cloud Run").listen(process.env.PORT ?? 8080);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
